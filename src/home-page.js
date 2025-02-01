ea231463f0ab6cb8fd92f8bc5433009bfe34c643
theMungai <mungaithesimon@gmail.com>
2025-02-01 12:53:08 +0300

    Populated Home Page Dynamically

--- src/home-page.js
+++ src/home-page.js
@@ -1,1 +1,19 @@
-const homeContainer = 
+const heroContainer = document.querySelector("#content");
+const homeButton = document.querySelector(".home-btn");
+
+homeButton.addEventListener("click", () => {
+    homePage()
+})
+
+function homePage(){
+    heroContainer.innerHTML = ""
+    
+    let heroHTML = `
+        <div class="hero-text">
+                <h1>Welcome to Luxury Hotel</h1>
+                <p>A luxurious intimate escape where plush interiors and serene ambiance await. Perfect for those who want to feel wrapped in comfort.</p>
+            </div>
+    `;
+
+    heroContainer.innerHTML = heroHTML
+}
